a3233683ffed32e1e1acf396fc5bd3199fff3999
zhaoliangyu <Larry.zhao@amperetime.com>
2025-11-28 18:29:47 +0800

    项目结构重构

---
+++ database-assistant/src/main/java/com/zly/DatabaseAssistantApplication.java
@@ -0,0 +1,18 @@
+package com.zly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author zhaoliangyu
+ * @since 2025/11/28 18:16
+ */
+@SpringBootApplication
+public class DatabaseAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseAssistantApplication.class, args);
+    }
+}
---
+++ database-assistant/src/main/java/com/zly/config/ChatMemoryAutoConfiguration.java
@@ -0,0 +1,51 @@
+package com.zly.config;
+
+import com.alibaba.cloud.ai.memory.redis.RedisChatMemoryRepository;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatMemoryAutoConfiguration {
+//    @Bean
+//    @ConditionalOnMissingBean
+//    ChatMemoryRepository chatMemoryRepository() {
+//        return new InMemoryChatMemoryRepository();
+//    }
+//
+//    @Bean
+//    @ConditionalOnMissingBean
+//    ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
+//        return MessageWindowChatMemory.builder().chatMemoryRepository(chatMemoryRepository).build();
+//    }
+
+    @Value("${spring.ai.memory.redis.host}")
+    private String redisHost;
+    @Value("${spring.ai.memory.redis.port}")
+    private int redisPort;
+    @Value("${spring.ai.memory.redis.password}")
+    private String redisPassword;
+    @Value("${spring.ai.memory.redis.timeout}")
+    private int redisTimeout;
+
+    @Bean
+    public RedisChatMemoryRepository redisChatMemoryRepository() {
+        return RedisChatMemoryRepository.builder()
+                .host(redisHost)
+                .port(redisPort)
+                // 若没有设置密码则注释该项
+                // .password(redisPassword)
+                .timeout(redisTimeout)
+                .build();
+    }
+
+    @Bean
+    ChatMemory chatMemory(RedisChatMemoryRepository chatMemoryRepository) {
+        return MessageWindowChatMemory
+                .builder()
+                .maxMessages(10)
+                .chatMemoryRepository(chatMemoryRepository).build();
+    }
+}
